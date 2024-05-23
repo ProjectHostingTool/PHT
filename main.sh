fbcab2ce8b18a0b442e2f2df36ed424da63f673d
the-abra <aegean.sword1@gmail.com>
2024-05-23 22:41:02 +0300

    p

--- main.sh
+++ main.sh
@@ -12,6 +12,8 @@ source src/colors.sh
 source src/functions.sh
 source config.cfg
 
+[[ "$1" =~ ^(-V|-v) ]] && echo -e "$VERSION" && exit 0
+
 case "$1" in
     "get")       source core/moduleGet.sh     ;;
     "run")       source core/moduleRun.sh     ;;
