86dbb73984fd0a79f3d9649cc7d07c233de29597
the-abra <aegean.sword1@gmail.com>
2024-05-23 22:39:24 +0300

    p

--- setup/setup.sh
+++ setup/setup.sh
@@ -121,7 +121,7 @@ rm /tmp/colors.sh
 
 cd PHT/
 git rm --cached core/modules/staticIp.list
-git rm --cached -r core/modules/
+git rm -r --cached  core/modules/
 
 successlog "System files installed."
 exit 0
