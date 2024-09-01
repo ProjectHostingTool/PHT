0b8cadd0c71b9ab0f6cbe125c68ff33ed0e39676
the-abra <aegean.sword1@gmail.com>
2024-09-01 23:46:41 +0300

    recreated algorithm

--- core/moduleRun.sh
+++ core/moduleRun.sh
@@ -2,8 +2,10 @@ modulename="$2"
 params=("$@")
 [[ -z $modulename ]] && errorlog "You have to set module name!" && exit 1
 ! [[ -f "core/modules/confs/$modulename.conf" ]] && errorlog "Module conf not found!" && sublog "/opt/PHT/core/modules/confs/$modulename.conf" && exit 1
+! [[ -d "core/modules/$modulename" ]] && errorlog "Module path not found!"      && sublog "/opt/PHT/core/modules/$modulename" && exit 1
 
 confindex "core/modules/confs/$modulename.conf"
+[[ $(docker ps -as | grep "$id" | awk '{print $1}') != "$id" ]] && errorlog "Docker ID not match!"   && sublog "Reference ID -> $id" && exit 1
 
 if [[ "$3" =~ ("-t"|"--terminal"|"terminal"|"-c"|"--console"|"console") ]];then 
     [[ -z "${params[@]:3}" ]] && warnlog "You must set arguments." && sublog "Ex: bash, cat <args>" && exit 1
@@ -19,11 +21,8 @@ if [[ "$3" =~ ("-t"|"--terminal"|"terminal"|"-c"|"--console"|"console") ]];then
     exit 0
 fi
 
-! [[ -d "core/modules/$modulename" ]] && errorlog "Module path not found!"      && sublog "/opt/PHT/core/modules/$modulename" && exit 1
 ! [[ -f "core/modules/$name/$exec" ]] && errorlog "Module exec file not found!" && sublog "/opt/PHT/core/modules/$modulename/$exec" && exit 1
-
-[[ $(docker ps -as | grep "$id" | awk '{print $1}') != "$id" ]] && errorlog "Docker ID not match!"   && sublog "Reference ID -> $id" && exit 1
-[[ $(docker ps -as | grep "$id") =~ ("up"|"UP"|"Up") ]]         && infolog "System already running." && exit 0
+[[ $(docker ps -as | grep "$id") =~ ("up"|"UP"|"Up") ]] && infolog "System already running."  && sublog "if you want to connect use '--console' parameter." && exit 0
 
 docker start $id 1> /tmp/phtrun.error || { errorlog "Something went wrong during starting $id!" && exit 1; }
 docker exec -it $id bash -c "cd $vpath && bash $vpath/$exec" ${params[@]:2} || { errorlog "Something went wrong during running $id!" && exit 1; }
