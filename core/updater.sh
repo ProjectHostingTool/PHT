3d96e6fcad8a5713693f68a263d8e95f27d0d898
the-abra <aegean.sword1@gmail.com>
2024-11-17 20:51:58 +0300

    UP.6

--- core/updater.sh
+++ core/updater.sh
@@ -2,10 +2,8 @@ echo -e "${CYAN}|----------[PULL | MAIN]----------|${RESET_COLOR}"
 git pull origin main 1> /dev/null 2> /tmp/pht-update.log
 
 if [[ $? == 0 ]]; then
-    stopanimation "done"
     log.submessage "PHT is Up to date"
 else
-    stopanimation "error"
     log.submessage "Update Fail."
     echo -ne "Do you want to view process log? (y/N)"
     read -e updatelogchoie
