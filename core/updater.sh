25394b8ed80935700954b9dc3c16f4de0d4197e5
the-abra <aegean.sword1@gmail.com>
2024-11-17 18:55:39 +0300

    UP.1

--- core/updater.sh
+++ core/updater.sh
@@ -1,2 +1,14 @@
 echo -e "${CYAN}|----------[PULL | MAIN]----------|${RESET_COLOR}"
-git pull origin main
+git pull origin main &> /dev/null
+
+if [[ $? == 0 ]]; then
+    stopanimation "done"
+    log.submessage "$name is Up to date"
+else
+    stopanimation "error"
+    log.submessage "Update Fail."
+    echo -ne "Do you want to view process log? (y/N)"
+    read -e updatelogchoie
+    [[ $updatelogchoie =~ ^(y|Y) ]] && cat "/tmp/$name-update.log" | less
+    exit 1
+fi
