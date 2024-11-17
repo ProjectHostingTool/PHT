a39db276d6f5775d3caf7993f34210897eeb1fec
the-abra <aegean.sword1@gmail.com>
2024-11-17 18:56:30 +0300

    UP.2

--- core/updater.sh
+++ core/updater.sh
@@ -1,14 +1,14 @@
 echo -e "${CYAN}|----------[PULL | MAIN]----------|${RESET_COLOR}"
-git pull origin main &> /dev/null
+git pull origin main 1> /dev/null 2> /tmp/pht-update.log
 
 if [[ $? == 0 ]]; then
     stopanimation "done"
-    log.submessage "$name is Up to date"
+    log.submessage "PHT is Up to date"
 else
     stopanimation "error"
     log.submessage "Update Fail."
     echo -ne "Do you want to view process log? (y/N)"
     read -e updatelogchoie
-    [[ $updatelogchoie =~ ^(y|Y) ]] && cat "/tmp/$name-update.log" | less
+    [[ $updatelogchoie =~ ^(y|Y) ]] && cat "/tmp/pht-update.log" | less
     exit 1
 fi
